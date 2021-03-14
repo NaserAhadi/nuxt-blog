4570b7a504bd49a8ebb062d540a89b4464423219
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2021-03-14 20:09:09 +0330

    2nd : Create about page

---
+++ pages/about/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="about-page">
+        <h1>This is my absolutely fantastic "About Me" Page!</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, quisquam?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, quisquam?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, quisquam?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, quisquam?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, quisquam?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, quisquam?</p>
+    </div>
+</template>
+
+<style scoped>
+.about-page{
+    text-align:center;
+    padding: 30px;
+}
+</style>
