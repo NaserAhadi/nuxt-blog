f392e7cf99a2fb01d0868f8ff401043085bc804b
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2021-03-18 23:05:49 +0330

    4th : Creat Post List component

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,49 @@
+<template>
+    <section class="post-list">
+      <PostPreview 
+        id="1"
+        title="Post One"
+        previewText="This is First post"
+        thumbnail="tech1.png"
+      />
+      <PostPreview 
+        id="2"
+        title="Post Two"
+        previewText="This is Seconde post"
+        thumbnail="tech2.png"
+      />
+      <PostPreview 
+        id="3"
+        title="Post Three"
+        previewText="This is Third post"
+        thumbnail="tech3.png"
+      />
+    </section>
+</template>
+
+<script>
+import PostPreview from "@/components/Posts/PostPreview"
+export default {
+  components:{
+    PostPreview
+  }
+}
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column !important;
+}
+@media (min-width:850px){
+  .post-list{
+    flex-direction: row !important;
+    justify-content: space-around;
+  }
+}
+</style>
