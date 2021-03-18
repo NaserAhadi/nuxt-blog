c180325fcc8e9d5aac915cc973972abbbd63451c
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2021-03-18 22:46:43 +0330

    4th : Add navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,40 @@
 <template>
   <div>
+    <TheHeader 
+      @sidenavToggle="toggleSidenav"
+    />
+    <TheSidenav 
+      :show="displaySidenav"
+      @close="closeSidenav"
+    />
     <Nuxt />
   </div>
 </template>
+<script>
+import TheHeader from "@/components/Navigation/TheHeader"
+import TheSidenav from "@/components/Navigation/TheSidenav"
+export default {
+  components:{
+    TheHeader,
+    TheSidenav
+  },
+  data(){
+    return {
+      displaySidenav:false
+    }
+  },
+  methods:{
+    toggleSidenav(){
+      this.displaySidenav = !this.displaySidenav
+      console.log(this.displaySidenav)
+
+    },
+    closeSidenav(){
+      this.displaySidenav = false
+    }
+  }
+}
+</script>
 
 <style>
 html {
