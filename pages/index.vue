0f32dd321646cc3b06f91ab313848a8af0df9a9c
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2021-03-18 21:12:37 +0330

    4th : Add background image to cover

--- pages/index.vue
+++ pages/index.vue
@@ -41,6 +41,7 @@ export default {
   position: relative;
   padding: 30px;
   box-sizing: border-box;
+  background-image: url("@/assets/image/main-page-background.jpg");
   background-position: center;
   background-size: cover;
 }
