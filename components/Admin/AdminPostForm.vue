c814934716e44a6e92589edf4b69df5046865ff2
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2021-03-21 19:14:51 +0330

    5th : Create admin post form

---
+++ components/Admin/AdminPostForm.vue
@@ -0,0 +1,79 @@
+<template>
+    <form @submit.prevent="onSave">
+        <AppControlInput
+            v-model="editedPost.author"
+        >
+            Author Name
+        </AppControlInput>
+        <AppControlInput
+            v-model="editedPost.title"
+        >
+            Title
+        </AppControlInput>
+        <AppControlInput
+            v-model="editedPost.thumbnail"
+        >
+            Thumbnail
+        </AppControlInput>
+        <AppControlInput
+            control-type="textarea"
+            v-model="editedPost.content"
+        >
+            Content
+        </AppControlInput>
+        <AppButton type="submit">Save</AppButton>
+        <AppButton
+            type="button"
+            style="margin-left: 10px"
+            btn-style="cancel"
+            @click="onCancel"
+        >
+            Cancel
+        </AppButton>
+    </form>
+</template>
+
+<script>
+import AppButton from "@/components/UI/AppButton"
+import AppControlInput from "@/components/UI/AppControlInput"
+
+export default {
+    components:{
+        AppButton,
+        AppControlInput
+    },
+    props:{
+        post:{
+            type:Object,
+            required:false
+        }
+    },
+    data(){
+        return {
+            editedPost:
+                this.post?
+                {...this.post}:
+                {
+                author:"",
+                title:"",
+                content:"",
+                thumbnail:""
+                }
+        }
+    },
+    mounted(){
+        console.log(this.editedPost)
+    },
+    methods:{
+        onSave(){
+            // Save the post
+            console.log(this.editedPost)
+        },
+        onCancel(){
+            // navigate back
+            this.$router.push("/admin")
+        }
+    }
+}
+</script>
+
