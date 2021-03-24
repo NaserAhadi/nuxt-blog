8fd2e0a973a0af765b5ef49c3f18d328c70caf1f
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2021-03-24 21:55:13 +0430

    7th : Using nuxtServerInit for initialize store

--- store/index.js
+++ store/index.js
@@ -10,6 +10,33 @@ const createStore = () => {
             SET_POSTS:(state, payload) => state.loadedPosts = payload
         },
         actions:{
+            nuxtServerInit(vuexContext, context){
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        vuexContext.commit("SET_POSTS", [
+                            {
+                              id:"1",
+                              title:"First Post",
+                              previewText:"This is First post",
+                              thumbnail:"tech1.png"
+                            },
+                            {
+                              id:"2",
+                              title:"Seconde Post",
+                              previewText:"This is seconde post",
+                              thumbnail:"tech2.png"
+                            },
+                            {
+                              id:"3",
+                              title:"Third Post",
+                              previewText:"This is third post",
+                              thumbnail:"tech3.png"
+                            }
+                          ])
+                        resolve();
+                    })
+                })
+            },
             setPosts(vuexContext, posts){
                 vuexContext.commit("SET_POSTS", posts)
             }
